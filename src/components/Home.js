852c412184e79ae0a68aa04c8e39a231e3b4754d
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-04 20:35:10 +0500

    updated supported network and added information in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,44 @@
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 function Home() {
+
+  const {
+    connector,
+    library,
+    account,
+    chainId,
+    activate,
+    deactivate,
+    active,
+    error
+  } = useWeb3React();
+  const [balance, setBalance] = useState();
+  useEffect(()=>{
+    (async ()=>{
+      if(library && account){
+        try {
+          const _balance = await library.getBalance(account);
+          setBalance(ethers.utils.formatEther(_balance));
+        }
+        catch(error){
+          console.log("error = ",error.message);
+          setBalance("0");
+        }
+        return () => {
+          setBalance(undefined);
+        };
+      }
+    })();
+  }, [library, account, chainId]);
+
   return (
     <div>
-      Hello World Home
+      <div>Hello Web3 World</div><br/>
+      <div>Chain Id: {chainId}</div><br/>
+      <div>Account: {account}</div><br/>
+      <div>Balance: {balance}</div>
     </div>
   );
 }
--- src/utils/connectors.js
+++ src/utils/connectors.js
@@ -3,9 +3,9 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 export const injectedConnector = new InjectedConnector({
     supportedChainIds: [
         //1, // Mainet
-        // 3, // Ropsten
+        3, // Ropsten
         4, // Rinkeby
         // 5, // Goerli
-        // 42, // Kovan
+        //42, // Kovan
     ]
 })
